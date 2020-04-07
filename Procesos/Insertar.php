109c63d1988d66ee7019e38a1adee7002eb3f146
Armandot1998 <armandotcajilema@gmailcom>
2020-04-07 09:42:10 -0500

    Initial commit

---
+++ Procesos/Insertar.php
@@ -0,0 +1,12 @@
+<?php
+
+$strCedula = $_POST['cedula'];
+
+echo '<div class="alert alert-danger" role="alert">
+<strong>Cédula Incorrecta!</strong> revise e ingrese una cédula correcta por favor
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+</button>
+</div>';
+
+?>
